750cfbcfbb9e676a8f0a82470abea07a16865e05
aterai <at.terai@gmail.com>
2014-10-13 18:42:03 +0900

    add: plugin_flair

---
+++ plugin/flair.inc.php
@@ -0,0 +1,10 @@
+<?php
+//-*- mode:java; Encoding:utf8n -*-
+function plugin_flair_inline() {
+return <<<EOD
+<a href="http://stackoverflow.com/users/177145/aterai">
+<img src="http://stackoverflow.com/users/flair/177145.png?theme=clean" width="208" height="58" alt="profile for aterai at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for aterai at Stack Overflow, Q&amp;A for professional and enthusiast programmers">
+</a>
+EOD;
+}
+?>
