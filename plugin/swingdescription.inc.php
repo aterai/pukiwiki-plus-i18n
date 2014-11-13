5796515587ff768473f5ec40a70937ca8275e7d1
aterai <at.terai@gmail.com>
2014-11-13 22:18:05 +0900

    itemprop, image

--- plugin/swingdescription.inc.php
+++ plugin/swingdescription.inc.php
@@ -17,15 +17,31 @@ function plugin_swingdescription_convert() {
 
     return <<<EOD
 <div class="row">
-<div class="col-md-10 col-xs-12">
-<p>$description</p>
-<ul><li><a href="$paurl" title="$page">$title</a></li></ul>
-</div>
-<div class="col-md-2 col-xs-12">
-<a href="$paurl" title="$page"><img src="$ssurl" width="144" height="120" alt="thumbnail" /></a>
-</div>
+  <div class="col-md-2 col-xs-12">
+    <a href="$paurl" class="thumbnail" title="$page">
+      <img src="$ssurl" itemprop="image" alt="thumbnail" />
+    </a>
+  </div>
+  <div class="col-md-10 col-xs-12 caption">
+    <h4 class="caption"><a href="$paurl" title="$page">$title</a></h4>
+    <p>$description</p>
+  </div>
 </div>
 EOD;
+
+// <div class="row">
+// <div class="col-md-2 col-xs-12">
+// <a href="$paurl" class="thumbnail" title="$page"><img src="$ssurl" itemprop="image" alt="thumbnail" /></a>
+// </div>
+// <div class="col-md-10 col-xs-12">
+// <div class="caption">
+// <a href="$paurl" title="$page">$title</a>
+// <hr />
+// <p>$description</p>
+// </div>
+// </div>
+// </div>
+// EOD;
 //     return <<<EOD
 // <div class="col-md-6 col-xs-12">
 // <div class="thumbnail">
