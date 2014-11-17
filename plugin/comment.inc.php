039d6645d4a11122d86b3d5b3136f889bb2dca23
aterai <at.terai@gmail.com>
2014-11-17 21:10:44 +0900

    livefyre, gtm

--- plugin/comment.inc.php
+++ plugin/comment.inc.php
@@ -210,23 +210,6 @@ function plugin_comment_convert()
 <br />
 
 <div id="livefyre-comments"></div>
-<script type="text/javascript" src="http://zor.livefyre.com/wjs/v3.0/javascripts/livefyre.js"></script>
-<script type="text/javascript">
-(function () {
-    var articleId = fyre.conv.load.makeArticleId(null);
-    fyre.conv.load({}, [{
-        el: 'livefyre-comments',
-        network: "livefyre.com",
-        siteId: "367661",
-        articleId: articleId,
-        signed: false,
-        collectionMeta: {
-            articleId: articleId,
-            url: fyre.conv.load.makeCollectionUrl(),
-        }
-    }], function() {});
-}());
-</script>
 
 </article>
 <br />
